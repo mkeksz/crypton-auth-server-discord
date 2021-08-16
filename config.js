c759d5e252387c333d8051dbc350a02165411434
mrkeksz <mrkeksz@yandex.ru>
2021-08-16 15:55:35 +0300

    update

--- config.js
+++ config.js
@@ -4,12 +4,13 @@ dotenv.config()
 const config = {
     REDIS_HOST: process.env['REDIS_HOST'],
     REDIS_PORT: process.env['REDIS_PORT'],
-    REDIS_AUTH_PATH: process.env['REDIS_AUTH_PATH'],
-    CLIENT_ID: process.env['CLIENT_ID'],
-    CLIENT_SECRET: process.env['CLIENT_SECRET'],
-    PORT: process.env['PORT'],
-    HOST: process.env['HOST'],
-    DISCORD_INVITE_LINK: process.env['DISCORD_INVITE_LINK']
+    REDIS_AUTH_PATH: process.env['REDIS_AUTH_PASS'],
+    DISCORD_INVITE_LINK: process.env['DISCORD_INVITE_LINK'],
+    OAUTH2_CLIENT_ID: process.env['OAUTH2_CLIENT_ID'],
+    OAUTH2_CLIENT_SECRET: process.env['OAUTH2_CLIENT_SECRET'],
+    OAUTH2_REDIRECT_URI: process.env['OAUTH2_REDIRECT_URI'],
+    SSL_CERTIFICATE: process.env['SSL_CERTIFICATE'],
+    SSL_PRIVATE_KEY: process.env['SSL_PRIVATE_KEY']
 }
 
 module.exports = config
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const redis = require('redis')
+const fs = require('fs')
+const http = require('http')
+const https = require('https')
 const fetch = require('node-fetch')
 const express = require('express')
 const config = require('./config')
 
+const redisClient = redis.createClient(config.REDIS_PORT, config.REDIS_HOST, {auth_pass: config.REDIS_AUTH_PATH})
 const app = express()
-const redisClient = redis.createClient(config.REDIS_HOST, config.REDIS_PORT, {auth_pass: config.REDIS_AUTH_PATH})
 
 app.get('/', async ({query}, response) => {
     const {code, state} = query
@@ -18,10 +21,10 @@ app.get('/', async ({query}, response) => {
         const oauthResult = await fetch('https://discord.com/api/oauth2/token', {
             method: 'POST',
             body: new URLSearchParams({
-                client_id: config.CLIENT_ID,
-                client_secret: config.CLIENT_SECRET,
+                client_id: config.OAUTH2_CLIENT_ID,
+                client_secret: config.OAUTH2_CLIENT_SECRET,
                 grant_type: 'authorization_code',
-                redirect_uri: `http://${config.HOST}:${config.PORT}`,
+                redirect_uri: config.OAUTH2_REDIRECT_URI,
                 scope: 'identify',
                 code
             }),
@@ -39,10 +42,20 @@ app.get('/', async ({query}, response) => {
         const discord_id = user['id']
         const telegram_id = state
         const dataToSend = JSON.stringify({discord_id, telegram_id})
-        console.log(dataToSend, 'send to redis')
+        console.info('Auth data:', dataToSend)
         redisClient.publish('discordAuth', dataToSend)
     } catch (error) {console.error(error)}
     return response.redirect(config.DISCORD_INVITE_LINK)
 })
 
-app.listen(config.PORT, () => console.log(`App listening at http://localhost:${config.PORT}`))
+const httpServer = http.createServer(app)
+httpServer.listen(8080)
+console.info(`Launched`)
+
+if (config.SSL_CERTIFICATE && config.SSL_PRIVATE_KEY) {
+    const privateKey  = fs.readFileSync(config.SSL_PRIVATE_KEY, 'utf8')
+    const certificate = fs.readFileSync(config.SSL_CERTIFICATE, 'utf8')
+    const credentials = {cert: certificate, key: privateKey}
+    const httpsServer = https.createServer(credentials, app)
+    httpsServer.listen(443)
+}
